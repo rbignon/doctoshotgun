dd8a2407b9ee85f0f523b1c8c8cf5a808bfb5eb2
Edgar Lubicz <edgarlubicz@gmail.com>
2021-06-06 14:31:34 +0200

    Adding country support. Now France (fr) and Germany are available (de) Adding Janssen vaccine option Adjusting test cases and readme Retry after 5s in case of failure in connection In case on many vaccine types, try all of the motives instead of only one.

--- test_browser.py
+++ test_browser.py
@@ -1,21 +1,20 @@
 import pytest
 import responses
 from woob.browser.exceptions import ServerError
-
-from doctoshotgun import Doctolib
+from doctoshotgun import DoctolibDE, DoctolibFR
 
 
 @responses.activate
-def test_find_centers_returns_503_should_continue(tmp_path):
+def test_find_centers_fr_returns_503_should_continue(tmp_path):
     """
     Check that find_centers doesn't raise a ServerError in case of 503 HTTP response
     """
-    docto = Doctolib("roger.phillibert@gmail.com", "1234", responses_dirname=tmp_path)
+    docto = DoctolibFR("roger.phillibert@gmail.com", "1234", responses_dirname=tmp_path)
     docto.BASEURL = "https://127.0.0.1"
 
     responses.add(
         responses.GET,
-        "https://127.0.0.1/vaccination-covid-19/Paris?ref_visit_motive_ids%5B%5D=6970&ref_visit_motive_ids%5B%5D=7005",
+        "https://127.0.0.1/vaccination-covid-19/Paris?ref_visit_motive_ids%5B%5D=6970&ref_visit_motive_ids%5B%5D=7005&ref_visit_motive_ids%5B%5D=7945",
         status=503
     )
 
@@ -23,22 +22,59 @@ def test_find_centers_returns_503_should_continue(tmp_path):
     for _ in docto.find_centers(["Paris"]):
         pass
 
-
 @responses.activate
-def test_find_centers_returns_502_should_fail(tmp_path):
+def test_find_centers_de_returns_503_should_continue(tmp_path):
     """
-    Check that find_centers raises an error in case of non-whitelisted status code
+    Check that find_centers doesn't raise a ServerError in case of 503 HTTP response
     """
-    docto = Doctolib("roger.phillibert@gmail.com", "1234", responses_dirname=tmp_path)
+    docto = DoctolibDE("roger.phillibert@gmail.com", "1234", responses_dirname=tmp_path)
     docto.BASEURL = "https://127.0.0.1"
 
     responses.add(
         responses.GET,
-        "https://127.0.0.1/vaccination-covid-19/Paris?ref_visit_motive_ids%5B%5D=6970&ref_visit_motive_ids%5B%5D=7005",
+        "https://127.0.0.1/impfung-covid-19-corona/M%C3%BCnchen?ref_visit_motive_ids%5B%5D=6768&ref_visit_motive_ids%5B%5D=6936&ref_visit_motive_ids%5B%5D=7978",
+        status=503
+    )
+
+    # this should not raise an exception
+    for _ in docto.find_centers(["München"]):
+        pass
+
+
+@responses.activate
+def test_find_centers_fr_returns_502_should_fail(tmp_path):
+    """
+    Check that find_centers raises an error in case of non-whitelisted status code
+    """
+    docto = DoctolibFR("roger.phillibert@gmail.com", "1234", responses_dirname=tmp_path)
+    docto.BASEURL = "https://127.0.0.1"
+
+    responses.add(
+        responses.GET,
+        "https://127.0.0.1/vaccination-covid-19/Paris?ref_visit_motive_ids%5B%5D=6970&ref_visit_motive_ids%5B%5D=7005&ref_visit_motive_ids%5B%5D=7945",
         status=502
     )
 
     # this should raise an exception
     with pytest.raises(ServerError):
         for _ in docto.find_centers(["Paris"]):
+            pass
+
+@responses.activate
+def test_find_centers_de_returns_502_should_fail(tmp_path):
+    """
+    Check that find_centers raises an error in case of non-whitelisted status code
+    """
+    docto = DoctolibDE("roger.phillibert@gmail.com", "1234", responses_dirname=tmp_path)
+    docto.BASEURL = "https://127.0.0.1"
+
+    responses.add(
+        responses.GET,
+        "https://127.0.0.1/impfung-covid-19-corona/M%C3%BCnchen?ref_visit_motive_ids%5B%5D=6768&ref_visit_motive_ids%5B%5D=6936&ref_visit_motive_ids%5B%5D=7978",
+        status=502
+    )
+
+    # this should raise an exception
+    with pytest.raises(ServerError):
+        for _ in docto.find_centers(["München"]):
             pass
