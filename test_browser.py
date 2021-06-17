beae68dce422110f9ecf912154839c44d7fe8595
gitolicious <26963495+gitolicious@users.noreply.github.com>
2021-06-17 20:27:54 +0000

    add try_to_book happy path test with mock responses

--- test_browser.py
+++ test_browser.py
@@ -1,7 +1,13 @@
 import pytest
 import responses
+from html import escape
+import json
+import datetime
 from woob.browser.exceptions import ServerError
 from doctoshotgun import DoctolibDE, DoctolibFR
+
+# globals
+FIXTURES_FOLDER = "test_fixtures"
 
 
 @responses.activate
@@ -78,3 +84,156 @@ def test_find_centers_de_returns_502_should_fail(tmp_path):
     with pytest.raises(ServerError):
         for _ in docto.find_centers(["München"]):
             pass
+
+
+@responses.activate
+def test_book_slots_should_succeed(tmp_path):
+    """
+    Check that try_to_book calls all services successfully
+    """
+    docto = DoctolibDE("roger.phillibert@gmail.com", "1234", responses_dirname=tmp_path)
+    docto.BASEURL = "https://127.0.0.1"
+    docto.patient = {
+        "id": "patient-id",
+        "first_name": "Roger",
+        "last_name": "Phillibert"
+    }
+
+    mockSearchResultId = {
+        "searchResultId": 1234567
+    }
+
+    mockSearchResultIdEscapedJSON = escape(json.dumps(mockSearchResultId, separators=(',', ':')))
+
+    responses.add(
+        responses.GET,
+        "https://127.0.0.1/impfung-covid-19-corona/K%C3%B6ln?ref_visit_motive_ids%5B%5D=6768&ref_visit_motive_ids%5B%5D=6936&ref_visit_motive_ids%5B%5D=7978",
+        status=200,
+        body="<div class='js-dl-search-results-calendar' data-props='{dataProps}'></div>".format(
+            dataProps=mockSearchResultIdEscapedJSON)
+    )
+
+    with open(FIXTURES_FOLDER + '/search_result.json') as json_file:
+        mockSearchResult = json.load(json_file)
+
+        responses.add(
+            responses.GET,
+            "https://127.0.0.1/search_results/1234567.json?limit=4&ref_visit_motive_ids%5B%5D=6768&ref_visit_motive_ids%5B%5D=6936&ref_visit_motive_ids%5B%5D=7978&speciality_id=5494&search_result_format=json",
+            status=200,
+            body=json.dumps(mockSearchResult)
+        )
+
+    with open(FIXTURES_FOLDER + '/doctor_response.json') as json_file:
+        mockDoctorResponse = json.load(json_file)
+
+        responses.add(
+            responses.GET,
+            "https://127.0.0.1/allgemeinmedizin/koeln/dr-dre?insurance_sector=public",
+            status=200,
+            body=json.dumps(mockDoctorResponse)
+        )
+
+    responses.add(
+        responses.GET,
+        "https://127.0.0.1/booking/dr-dre.json",
+        status=200,
+        body=json.dumps(mockDoctorResponse)
+    )
+
+    with open(FIXTURES_FOLDER + '/availabilities.json') as json_file:
+        mockAvailabilities = json.load(json_file)
+
+        responses.add(
+            responses.GET,
+            "https://127.0.0.1/availabilities.json?start_date=2021-06-01&visit_motive_ids=2920448&agenda_ids=&insurance_sector=public&practice_ids=234567&destroy_temporary=true&limit=3",
+            status=200,
+            body=json.dumps(mockAvailabilities)
+        )
+        responses.add(
+            responses.GET,
+            "https://127.0.0.1/availabilities.json?start_date=2021-06-01&visit_motive_ids=2746983&agenda_ids=&insurance_sector=public&practice_ids=234567&destroy_temporary=true&limit=3",
+            status=200,
+            body=json.dumps(mockAvailabilities)
+        )
+
+    mockAppointments = {
+        "id": "appointment-id"
+    }
+
+    responses.add(
+        responses.POST,
+        "https://127.0.0.1/appointments.json",
+        status=200,
+        body=json.dumps(mockAppointments)
+    )
+
+    mockAppointmentsEdit = {
+        "id": "appointment-edit-id",
+        "appointment": {
+            "custom_fields": {}
+        }
+    }
+
+    responses.add(
+        responses.GET,
+        "https://127.0.0.1/appointments/appointment-id/edit.json",
+        status=200,
+        body=json.dumps(mockAppointmentsEdit)
+    )
+
+    responses.add(
+        responses.GET,
+        "https://127.0.0.1/second_shot_availabilities.json?start_date=2021-07-20&visit_motive_ids=2746983&agenda_ids=&first_slot=2021-06-10T08%3A40%3A00.000%2B02%3A00&insurance_sector=public&practice_ids=234567&limit=3",
+        status=200,
+        body=json.dumps(mockAvailabilities)
+    )
+
+    mockAppointmentIdPut = {
+    }
+
+    responses.add(
+        responses.PUT,
+        "https://127.0.0.1/appointments/appointment-id.json",
+        status=200,
+        body=json.dumps(mockAppointmentIdPut)
+    )
+
+    mockAppointmentId = {
+        "confirmed": True
+    }
+
+    responses.add(
+        responses.GET,
+        "https://127.0.0.1/appointments/appointment-id.json",
+        status=200,
+        body=json.dumps(mockAppointmentId)
+    )
+
+    resultHandled = False
+    for result in docto.find_centers(["Köln"]):
+        resultHandled = True
+
+        center = result['search_result']
+
+        # single shot vaccination
+        assert docto.try_to_book(center=center,
+                                 vaccine_list=["Janssen"],
+                                 start_date=datetime.date(
+                                     year=2021, month=6, day=1),
+                                 end_date=datetime.date(
+                                     year=2021, month=6, day=14),
+                                 dry_run=False)
+        assert len(responses.calls) == 10
+
+        # two shot vaccination
+        assert docto.try_to_book(center=center,
+                                 vaccine_list=["Pfizer"],
+                                 start_date=datetime.date(
+                                     year=2021, month=6, day=1),
+                                 end_date=datetime.date(
+                                     year=2021, month=6, day=14),
+                                 dry_run=False)
+        assert len(responses.calls) == 20
+        pass
+
+    assert resultHandled
