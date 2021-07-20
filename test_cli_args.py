991f1af0baf9e6835501e183ccce879cb0661bb6
Elliott deLaunay <edelauna@gmail.com>
2021-07-20 19:11:52 -0400

    updating test cases for new classes

--- test_cli_args.py
+++ test_cli_args.py
@@ -1,7 +1,7 @@
 import responses
 from unittest.mock import patch, MagicMock
 
-from doctoshotgun import Application, DoctolibDE, DoctolibFR, MasterPatientPage
+from doctoshotgun import Application, DoctolibDE, DoctolibFR, MasterPatientPage, Pfizer, Moderna, Janssen
 
 CENTERS = [
     {
@@ -68,9 +68,11 @@ def get_mocked_doctolib(MockDoctolibDE):
     MockDoctolibDE.return_value = mock_doctolib_de
 
     mock_doctolib_de.vaccine_motives = DoctolibDE.vaccine_motives
-    mock_doctolib_de.KEY_PFIZER = DoctolibDE.KEY_PFIZER
-    mock_doctolib_de.KEY_MODERNA = DoctolibDE.KEY_MODERNA
-    mock_doctolib_de.KEY_JANSSEN = DoctolibDE.KEY_JANSSEN
+    mock_doctolib_de.VACCINES = {
+        'PFIZER' : Pfizer('6768', 'Pfizer'),
+        'MODERNA' : Moderna('6936', 'Moderna'),
+        'JANSSEN' : Janssen('7978','Janssen')
+    }
 
     mock_doctolib_de.get_patients.return_value = [
         {"first_name": 'First', "last_name": 'Name'}
