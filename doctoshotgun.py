a88a6daa2c76658145463a034efbd692dc22e602
gill-anupreetkaur-216675597 <gillanupreet12@gmail.com>
2021-07-24 09:58:30 -0400

    added function make_vaccine_motives

--- doctoshotgun.py
+++ doctoshotgun.py
@@ -60,54 +60,6 @@ def log_ts(text=None, *args, **kwargs):
     if text:
         log(text, *args, **kwargs)
 
-class Vaccine:
-    #aggregate root
-    def __init__(self, key, vaccine_motive):
-        self.key = key
-    #Vaccine's main identifier is key
-        self.motive = vaccine_motive
-    #each vaccine should have a vaccine_motive based on the key
-
-    vaccine_motives = {}
-
- #motive_ids are extracted based on the vaccine names to book a vaccine
-
-    def try_to_book(self, center, vaccine_list, start_date, end_date, only_second, only_third, dry_run=False):
-        self.open(center['url'])
-        p = urlparse(center['url'])
-        center_id = p.path.split('/')[-1]
-
-        center_page = self.center_booking.go(center_id=center_id)
-        profile_id = self.page.get_profile_id()
-        # extract motive ids based on the vaccine names
-        motives_id = dict()
-        for vaccine in vaccine_list:
-            motives_id[vaccine] = self.page.find_motive(
-                r'.*({})'.format(vaccine), singleShot=(vaccine == self.vaccine_motives[self.KEY_JANSSEN] or only_second or only_third))
-
-        motives_id = dict((k, v)
-                          for k, v in motives_id.items() if v is not None)
-        if len(motives_id.values()) == 0:
-            log('Unable to find requested vaccines in motives')
-            log('Motives: %s', ', '.join(self.page.get_motives()))
-            return False
-
-        for place in self.page.get_places():
-            if place['name']:
-                log('– %s...', place['name'])
-            practice_id = place['practice_ids'][0]
-            for vac_name, motive_id in motives_id.items():
-                log('  Vaccine %s...', vac_name, end=' ', flush=True)
-                agenda_ids = center_page.get_agenda_ids(motive_id, practice_id)
-                if len(agenda_ids) == 0:
-                    # do not filter to give a chance
-                    agenda_ids = center_page.get_agenda_ids(motive_id)
-
-                if self.try_to_book_place(profile_id, motive_id, practice_id, agenda_ids, vac_name.lower(), start_date, end_date, only_second, only_third, dry_run):
-                    return True
-
-        return False
-
 
 class Session(cloudscraper.CloudScraper):
     def send(self, *args, **kwargs):
@@ -562,10 +514,55 @@ class Doctolib(LoginBrowser):
 
         return self.page.doc['confirmed']
 
+class Vaccine:
+    #aggregate root
+    def __init__(self, key, motive):
+        self.key = key
+    #Vaccine's main identifier is key
+        self.motive = motive
+    # each vaccine has a motive
 
+    #vaccine_motives is a collection of vaccine_motive based on the vaccine_key
+    def make_vaccine_motives(self):
+        return {self.key: self.motive}
 
+ #motive_ids are extracted based on the vaccine names to book a vaccine
 
+    def try_to_book(self, center, vaccine_list, start_date, end_date, only_second, only_third, dry_run=False):
+        self.open(center['url'])
+        p = urlparse(center['url'])
+        center_id = p.path.split('/')[-1]
 
+        center_page = self.center_booking.go(center_id=center_id)
+        profile_id = self.page.get_profile_id()
+        # extract motive ids based on the vaccine names
+        motives_id = dict()
+        for vaccine in vaccine_list:
+            motives_id[vaccine] = self.page.find_motive(
+                r'.*({})'.format(vaccine), singleShot=(vaccine == self.vaccine_motives[self.KEY_JANSSEN] or only_second or only_third))
+
+        motives_id = dict((k, v)
+                          for k, v in motives_id.items() if v is not None)
+        if len(motives_id.values()) == 0:
+            log('Unable to find requested vaccines in motives')
+            log('Motives: %s', ', '.join(self.page.get_motives()))
+            return False
+
+        for place in self.page.get_places():
+            if place['name']:
+                log('– %s...', place['name'])
+            practice_id = place['practice_ids'][0]
+            for vac_name, motive_id in motives_id.items():
+                log('  Vaccine %s...', vac_name, end=' ', flush=True)
+                agenda_ids = center_page.get_agenda_ids(motive_id, practice_id)
+                if len(agenda_ids) == 0:
+                    # do not filter to give a chance
+                    agenda_ids = center_page.get_agenda_ids(motive_id)
+
+                if self.try_to_book_place(profile_id, motive_id, practice_id, agenda_ids, vac_name.lower(), start_date, end_date, only_second, only_third, dry_run):
+                    return True
+
+        return False
 
 
 class DoctolibDE(Doctolib):
