3060ee9552251ff631b70370885b1ba8683ff37e
Giusten-Annetta <giusten78@gmail.com>
2021-08-14 11:52:49 -0400

    Implemented the builder pattern to create Doctolib locales. Only the DE and FR locales have been implemented since they were the only ones that existed in the code.

--- doctoshotgun.py
+++ doctoshotgun.py
@@ -5,6 +5,7 @@ import logging
 import tempfile
 from time import sleep
 import json
+from typing import List
 from urllib.parse import urlparse
 import datetime
 import argparse
@@ -107,9 +108,10 @@ class CentersPage(HTMLPage):
             # JavaScript:
             # var t = (e = r()(e)).data("u")
             #     , n = atob(t.replace(/\s/g, '').split('').reverse().join(''));
-            
+
             import base64
-            href = base64.urlsafe_b64decode(''.join(span.attrib['data-u'].split())[::-1]).decode()
+            href = base64.urlsafe_b64decode(
+                ''.join(span.attrib['data-u'].split())[::-1]).decode()
             query = dict(parse.parse_qsl(parse.urlsplit(href).query))
 
             if 'page' in query:
@@ -121,8 +123,9 @@ class CentersPage(HTMLPage):
 
             if 'page' in query:
                 return int(query['page'])
-        
+
         return None
+
 
 class CenterResultPage(JsonPage):
     pass
@@ -547,58 +550,132 @@ class Doctolib(LoginBrowser):
 
         return self.page.doc['confirmed']
 
+    def set_locale(self, locale):
+        self.LOCALE = locale
 
-class DoctolibDE(Doctolib):
-    BASEURL = 'https://www.doctolib.de'
-    KEY_PFIZER = '6768'
-    KEY_PFIZER_SECOND = '6769'
-    KEY_PFIZER_THIRD = None
-    KEY_MODERNA = '6936'
-    KEY_MODERNA_SECOND = '6937'
-    KEY_MODERNA_THIRD = None
-    KEY_JANSSEN = '7978'
-    KEY_ASTRAZENECA = '7109'
-    KEY_ASTRAZENECA_SECOND = '7110'
-    vaccine_motives = {
-        KEY_PFIZER: 'Pfizer',
-        KEY_PFIZER_SECOND: 'Zweit.*Pfizer|Pfizer.*Zweit',
-        KEY_PFIZER_THIRD: 'Dritt.*Pfizer|Pfizer.*Dritt',
-        KEY_MODERNA: 'Moderna',
-        KEY_MODERNA_SECOND: 'Zweit.*Moderna|Moderna.*Zweit',
-        KEY_MODERNA_THIRD: 'Dritt.*Moderna|Moderna.*Dritt',
-        KEY_JANSSEN: 'Janssen',
-        KEY_ASTRAZENECA: 'AstraZeneca',
-        KEY_ASTRAZENECA_SECOND: 'Zweit.*AstraZeneca|AstraZeneca.*Zweit',
-    }
-    centers = URL(r'/impfung-covid-19-corona/(?P<where>\w+)', CentersPage)
-    center = URL(r'/praxis/.*', CenterPage)
+    def set_base_url(self, base_url):
+        self.BASEURL = base_url
+
+    def set_vaccine_keys(self, ids: List[str]):
+        self.KEY_PFIZER = ids[0]
+        self.KEY_PFIZER_SECOND = ids[1]
+        self.KEY_PFIZER_THIRD = ids[2]
+        self.KEY_MODERNA = ids[3]
+        self.KEY_MODERNA_SECOND = ids[4]
+        self.KEY_MODERNA_THIRD = ids[5]
+        self.KEY_JANSSEN = ids[6]
+        self.KEY_ASTRAZENECA = ids[7]
+        self.KEY_ASTRAZENECA_SECOND = ids[8]
+
+    def set_vaccine_motives(self, motives):
+        self.vaccine_motives = motives
+
+    def set_centers(self, regex):
+        self.centers = URL(regex, CentersPage)
+
+    def set_center(self, regex):
+        self.center = URL(regex, CenterPage)
 
 
-class DoctolibFR(Doctolib):
-    BASEURL = 'https://www.doctolib.fr'
-    KEY_PFIZER = '6970'
-    KEY_PFIZER_SECOND = '6971'
-    KEY_PFIZER_THIRD = '8192'
-    KEY_MODERNA = '7005'
-    KEY_MODERNA_SECOND = '7004'
-    KEY_MODERNA_THIRD = '8193'
-    KEY_JANSSEN = '7945'
-    KEY_ASTRAZENECA = '7107'
-    KEY_ASTRAZENECA_SECOND = '7108'
-    vaccine_motives = {
-        KEY_PFIZER: 'Pfizer',
-        KEY_PFIZER_SECOND: '2de.*Pfizer',
-        KEY_PFIZER_THIRD: '3e.*Pfizer',
-        KEY_MODERNA: 'Moderna',
-        KEY_MODERNA_SECOND: '2de.*Moderna',
-        KEY_MODERNA_THIRD: '3e.*Moderna',
-        KEY_JANSSEN: 'Janssen',
-        KEY_ASTRAZENECA: 'AstraZeneca',
-        KEY_ASTRAZENECA_SECOND: '2de.*AstraZeneca',
-    }
+class Director:
+    _builder = None
 
-    centers = URL(r'/vaccination-covid-19/(?P<where>\w+)', CentersPage)
-    center = URL(r'/centre-de-sante/.*', CenterPage)
+    def set_builder(self, builder):
+        self._builder = builder
+
+    def create_locale(self, *args, **kwargs) -> Doctolib:
+        if (self._builder == None):
+            raise Exception(
+                "Please assign a Builder before using the Director.")
+
+        locale = Doctolib(args, kwargs)
+
+        locale_str = self._builder._get_locale_str()
+        locale.set_locale(locale_str)
+
+        url = self._builder._get_base_url()
+        locale.set_base_url(url)
+
+        keys = self._builder._get_vaccine_keys()
+        locale.set_vaccine_keys(keys)
+
+        motives = self._builder._get_vaccine_motives()
+        locale.set_vaccine_motives(motives)
+
+        centers_regex = self._builder._get_centers_regex()
+        locale.set_centers(centers_regex)
+
+        center_regex = self._builder._get_center_regex()
+        locale.set_center(center_regex)
+
+        return locale
+
+
+class Builder:
+    def _get_locale_str(self): pass
+    def _get_base_url(self): pass
+    def _get_vaccine_keys(self): pass
+    def _get_vaccine_motives(self): pass
+    def _get_centers_regex(self): pass
+    def _get_center_regex(self): pass
+
+
+class DoctolibDE(Builder):
+    def _get_base_url(self):
+        return 'https://www.doctolib.de'
+
+    def _get_locale_str(self):
+        return 'de'
+
+    def _get_vaccine_keys(self):
+        keys = ['6768', '6769', None, '6936',
+                '6937', None, '7978', '7109', '7110']
+        return keys
+
+    def _get_vaccine_motives(self):
+        vaccine_motive_list = ['Pfizer', 'Zweit.*Pfizer|Pfizer.*Zweit', 'Dritt.*Pfizer|Pfizer.*Dritt', 'Moderna',
+                               'Zweit.*Moderna|Moderna.*Zweit', 'Dritt.*Moderna|Moderna.*Dritt', 'Janssen', 'AstraZeneca', 'Zweit.*AstraZeneca|AstraZeneca.*Zweit']
+        keys = self._get_vaccine_keys()
+        vaccine_motives = {}
+
+        for i in range(len(vaccine_motive_list)):
+            vaccine_motives[keys[i]] = vaccine_motive_list[i]
+        return vaccine_motives
+
+    def _get_centers_regex(self):
+        return r'/impfung-covid-19-corona/(?P<where>\w+)'
+
+    def _get_center_regex(self):
+        return r'/praxis/.*'
+
+
+class DoctolibFR(Builder):
+    def _get_base_url(self):
+        return 'https://www.doctolib.fr'
+
+    def _get_locale_str(self):
+        return 'fr'
+
+    def _get_vaccine_keys(self):
+        keys = ['6970', '6971', '8192', '7005',
+                '7004', '8193', '7945', '7107', '7108']
+        return keys
+
+    def _get_vaccine_motives(self):
+        vaccine_motive_list = ['Pfizer', '2de.*Pfizer', '3e.*Pfizer', 'Moderna',
+                               '2de.*Moderna', '3e.*Moderna', 'Janssen', 'AstraZeneca', '2de.*AstraZeneca']
+        keys = self._get_vaccine_keys()
+        vaccine_motives = {}
+
+        for i in range(len(vaccine_motive_list)):
+            vaccine_motives[keys[i]] = vaccine_motive_list[i]
+        return vaccine_motives
+
+    def _get_centers_regex(self):
+        return r'/vaccination-covid-19/(?P<where>\w+)'
+
+    def _get_center_regex(self):
+        return r'/centre-de-sante/.*'
 
 
 class Application:
@@ -620,9 +697,9 @@ class Application:
     def main(self, cli_args=None):
         colorama.init()  # needed for windows
 
-        doctolib_map = {
-            "fr": DoctolibFR,
-            "de": DoctolibDE
+        locale_builders = {
+            'fr': DoctolibFR(),
+            'de': DoctolibDE(),
         }
 
         parser = argparse.ArgumentParser(
@@ -662,7 +739,7 @@ class Application:
         parser.add_argument('--dry-run', action='store_true',
                             help='do not really book the slot')
         parser.add_argument(
-            'country', help='country where to book', choices=list(doctolib_map.keys()))
+            'country', help='country where to book', choices=list(locale_builders.keys()))
         parser.add_argument('city', help='city where to book')
         parser.add_argument('username', help='Doctolib username')
         parser.add_argument('password', nargs='?', help='Doctolib password')
@@ -681,8 +758,12 @@ class Application:
         if not args.password:
             args.password = getpass.getpass()
 
-        docto = doctolib_map[args.country](
+        director = Director()
+        director.set_builder(locale_builders[args.country])
+
+        docto = director.create_locale(
             args.username, args.password, responses_dirname=responses_dirname)
+
         if not docto.do_login(args.code):
             return 1
 
@@ -839,7 +920,8 @@ class Application:
                     sleep(SLEEP_INTERVAL_AFTER_CENTER)
 
                     log('')
-                log('No free slots found at selected centers. Trying another round in %s sec...', SLEEP_INTERVAL_AFTER_RUN)
+                log('No free slots found at selected centers. Trying another round in %s sec...',
+                    SLEEP_INTERVAL_AFTER_RUN)
                 sleep(SLEEP_INTERVAL_AFTER_RUN)
             except CityNotFound as e:
                 print('\n%s: City %s not found. Make sure you selected a city from the available countries.' % (
